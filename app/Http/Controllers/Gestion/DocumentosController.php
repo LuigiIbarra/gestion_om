e25e68892ec50eab415760d847dc9045738a6af6
Luis Ibarra <luis.ibarra@tsjcdmx.gob.mx>
2022-07-08 11:02:00 -0500

    Prueba 2 arreglar relacion importanciacontenido con documento

--- app/Http/Controllers/Gestion/DocumentosController.php
+++ app/Http/Controllers/Gestion/DocumentosController.php
@@ -47,7 +47,6 @@ class DocumentosController extends Controller
         } else {
             $data['documentos'] = Documento::with('tipodocumento','tipoanexo','estatusdocumento','prioridaddocumento','importanciacontenido','tema','tipoasunto','instruccion','personalremitente','personalconocimiento','destinatarioatencion','destinatarioconocimiento')->where('iestatus','=',1)->get();
         }
-        dd($data);
         return view('documentos.index',$data);
     }
 

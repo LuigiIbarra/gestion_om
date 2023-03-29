d1b03d96e70e76e7c4f99de51db438d000efe70d
Luis Ibarra <luis.ibarra@tsjcdmx.gob.mx>
2023-03-29 14:16:54 -0600

    Cargar nuevamente catálogos completos de Puestos, Areas y Personal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,14 +29,14 @@ class DatabaseSeeder extends Seeder
         $this->call(PuestosTableSeeder::class);
         $this->call(AdscripcionesTableSeeder::class);
         $this->call(PersonalTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermisosTableSeeder::class);
+        $this->call(UsuariosTableSeeder::class);
         /*
         $this->call(TiposAreasTableSeeder::class);
         $this->call(DocumentosTableSeeder::class);
         $this->call(ParametrosTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermisosTableSeeder::class);
         $this->call(PermisosRolesTableSeeder::class);
-        $this->call(UsuariosTableSeeder::class);
         */
         Model::reguard();
     }

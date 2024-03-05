d56cf614fcabdec8edeb6d4cb4058fa8448c3dae
Luis Ibarra <luis.ibarra@tsjcdmx.gob.mx>
2024-03-05 14:46:18 -0600

    Agregar Correspondencia A al Informe de Asuntos Pendientes.

--- app/Http/Controllers/Gestion/ReportesController.php
+++ app/Http/Controllers/Gestion/ReportesController.php
@@ -803,7 +803,7 @@ class ReportesController extends Controller
             $data['correspon_a']    = $request->correspon_a;
             $total_registros        = DB::table('tadocumentos as d')
                                             ->join('tcpersonal as p','d.iid_personal_remitente', '=', 'p.iid_personal')
-                                            ->join('tcpuestos as pst','p.iid_puesto', '=', 'pst.iid_puesto')
+                                            //->join('tcpuestos as pst','p.iid_puesto', '=', 'pst.iid_puesto')
                                             ->where('d.iid_estatus_documento','=',1)
                                             ->where('p.iid_adscripcion','=',$request->correpon_a)       //CORRESPONDENCIA A
                                             ->whereBetween('dfecha_recepcion',[$request->fecha_inicial,$request->fecha_final])
